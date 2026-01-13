b6919cd70b5b1adacb731c52e46682a114850541
welcome-magic-clock <welcome@magic-clock.com>
2026-01-13 11:46:01 +0100

    Update MagicDisplayPreviewShell.tsx

--- features/display/MagicDisplayPreviewShell.tsx
+++ features/display/MagicDisplayPreviewShell.tsx
@@ -889,25 +889,24 @@ editorOppositeSegment ? (
             )}
           </div>
 
-          {/* Ligne centrale */}
-          <div className="pointer-events-none absolute inset-y-3 left-1/2 w-[2px] -translate-x-1/2 bg-white/90" />
+                  {/* Trait central fin comme dans Magic Studio / FaceEditor */}
+          <div className="pointer-events-none absolute inset-y-0 left-1/2 w-px -translate-x-1/2 bg-slate-200" />
 
-          {/* Avatar centré (comme dans le Studio) */}
-          <div className="pointer-events-none absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2">
-            <div className="flex h-16 w-16 items-center justify-center rounded-full border border-white/90 bg-white shadow-md shadow-slate-900/30">
-              {creatorAvatar ? (
-                // eslint-disable-next-line @next/next/no-img-element
-                <img
-                  src={creatorAvatar}
-                  alt={creatorName}
-                  className="h-[54px] w-[54px] rounded-full object-cover"
-                />
-              ) : (
-                <span className="text-xs font-semibold text-slate-800">
-                  {creatorInitials}
-                </span>
-              )}
-            </div>
+          {/* Avatar centre – même style que Magic Studio / FaceEditor */}
+          <div className="pointer-events-none absolute left-1/2 top-1/2 z-20 flex h-20 w-20 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full border border-white/90 shadow-sm">
+            {creatorAvatar ? (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                src={creatorAvatar}
+                alt={creatorName}
+                className="h-[72px] w-[72px] rounded-full object-cover"
+              />
+            ) : (
+              <span className="text-base font-semibold text-white">
+                {creatorInitials}
+              </span>
+            )}
+          </div>
           </div>
         </div>
       </div>
