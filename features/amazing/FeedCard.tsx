cf43a744a211b03966b3d4813b13826f699fac97
welcome-magic-clock <welcome@magic-clock.com>
2026-02-11 15:14:48 +0100

    Make FeedCard clickable to detail page

--- features/amazing/FeedCard.tsx
+++ features/amazing/FeedCard.tsx
@@ -2,13 +2,18 @@
 
 import type { FeedCard as FeedCardType } from "@/core/domain/types";
 import MediaCard from "./MediaCard";
+import Link from "next/link";
 
 type Props = {
   item: FeedCardType;
 };
 
 export default function FeedCard({ item }: Props) {
-  // On délègue tout l’affichage à MediaCard,
-  // qui gère déjà l’avatar centré, le lock, les vues, etc.
-  return <MediaCard item={item} />;
+  // On délègue toujours l’affichage à MediaCard,
+  // mais on rend la carte cliquable vers la page de détail /p/[id].
+  return (
+    <Link href={`/p/${item.id}`} className="block">
+      <MediaCard item={item} />
+    </Link>
+  );
 }
