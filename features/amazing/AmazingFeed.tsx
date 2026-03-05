4e43f8d7a8ba3d11a6fa1db34591bec6a59ae423
welcome-magic-clock <welcome@magic-clock.com>
2026-03-05 10:11:57 +0100

    Add files via upload

---
+++ features/amazing/AmazingFeed.tsx
@@ -0,0 +1,19 @@
+// features/amazing/AmazingFeed.tsx
+"use client";
+
+import type { FeedCard } from "@/core/domain/types";
+import MediaCard from "./MediaCard";
+
+type Props = {
+  feed: FeedCard[];
+};
+
+export default function AmazingFeed({ feed }: Props) {
+  return (
+    <section className="flex flex-col gap-6 sm:gap-8 snap-y snap-mandatory">
+      {feed.map((item, idx) => (
+        <MediaCard key={`${String(item.id)}-${idx}`} item={item} />
+      ))}
+    </section>
+  );
+}
